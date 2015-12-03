983fe230e4054526f2fa75888ac2af98096685dc
Daiki_Surface <daisea3e1203@gmail.com>
2015-12-03 19:11:30 +0900

    fixed main, but problem with the clear screeen...

--- Cpp/Console/move.cpp
+++ Cpp/Console/move.cpp
@@ -10,24 +10,26 @@ int main() {
   KeyInput Key;
   system("cls");
   Char c(45,12,'@',COL_BLUE);
+  //bool run = true;
   while(1) {
     Key.UpdateKey();
     //clear screen
-    for (int i = 0; i < 80; i++) {
-      for (int j = 0; j < 25; i++) {
-        setPos(i,j);
-        cout << " ";
-      }
-    }
+    // for (int i = 0; i < 80; i++) {
+    //   for (int j = 0; j < 25; i++) {
+    //     setPos(i,j);
+    //     cout << " ";
+    //   }
+    // }
     if(Key.GetState(VK_RIGHT) >= 1) c.move(1,0);
     if(Key.GetState(VK_LEFT) >= 1) c.move(-1,0);
-    if(Key.GetState(VK_UP) >= 1) c.move(0,1);
-    if(Key.GetState(VK_DOWN) >= 1) c.move(0,-1);
+    if(Key.GetState(VK_UP) >= 1) c.move(0,-1);
+    if(Key.GetState(VK_DOWN) >= 1) c.move(0,1);
     c.draw();
 
-    if(Key.GetState('Q') >= 3) break;
-    Sleep(50);
+    if (Key.GetState('Q') >= 3) break;
+    Sleep(100);
   }
+  setColor(COL_WHITE);
   system("cls");
   return 0;
 }
